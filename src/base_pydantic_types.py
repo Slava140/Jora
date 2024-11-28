a149d4101519f8e6f3c0058e431c2c7c91fef170
Vyacheslav <slava20031112@gmail.com>
2024-11-28 09:33:46 +0500

    move 2 files to src

---
+++ src/base_pydantic_types.py
@@ -0,0 +1,38 @@
+import re
+from datetime import datetime, timezone
+from typing import Annotated
+
+from pydantic import Field, AfterValidator, BeforeValidator
+
+
+def is_utc_datetime_validator(value: datetime) -> datetime:
+    tz = value.tzinfo
+    if tz is timezone.utc:
+        return value
+    else:
+        raise ValueError('datetime must have a UTC timezone.')
+
+
+def password_validator(value: str) -> str:
+    if len(value) < 8:
+        raise ValueError('Password length must be greater or equal than 8')
+
+    if len(value) > 20:
+        raise ValueError('Password length must be less or equal than 20')
+
+    if not re.search(r'\d', value):
+        raise ValueError('Password must contain at least one digit')
+
+    if not re.search(r'[A-Z]', value):
+        raise ValueError('Password must contain at least one uppercase letter')
+
+    if not re.search(r'[a-z]', value):
+        raise ValueError('Password must contain at least one lowercase letter')
+
+    return value
+
+
+StrFrom3To255 = Annotated[str, Field(min_length=3, max_length=255)]
+
+UTCDatetime = Annotated[datetime, AfterValidator(is_utc_datetime_validator)]
+PasswordStr = Annotated[str, BeforeValidator(password_validator)]
